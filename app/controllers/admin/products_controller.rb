1006526b7ced16840ffe0be66fc8f6880e289ad8
mclmnop <m_charlebois@hotmail.com>
2021-03-31 15:01:58 -0400

    working with stored credentials, but need to save them first I think

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,5 +1,5 @@
 class Admin::ProductsController < ApplicationController
-  #http_basic_authenticate_with name: "Jungle", password: "book", except: [:index]
+  http_basic_authenticate_with name: Rails.configuration.admin[:username], password:  Rails.configuration.admin[:userpassword]
 
   def index
     @products = Product.order(id: :desc).all
--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -2,5 +2,9 @@ Rails.configuration.stripe = {
   :publishable_key => ENV['STRIPE_PUBLISHABLE_KEY'],
   :secret_key      => ENV['STRIPE_SECRET_KEY']
 }
+Rails.configuration.admin = {
+  :username => ENV['ADMIN_USERNAME'],
+  :userpassword => ENV['ADMIN_PASSWORD']
+}
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
